0bfab74cca7cabe4f6bf09df7c5b2b959987a6ba
Arthur <arthurquintino2304@gmail.com>
2025-10-07 22:22:34 -0300

    Versionamento inicial do projeto com o gitignore e um form basico de login

---
+++ IN-TEGRA/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace IN_TEGRA.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
